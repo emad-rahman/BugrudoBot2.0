920b4f8f159854cf04b4f5956c754b36aedcb10e
Emad Rahman <emadpoke@gmail.com>
2021-06-12 17:16:03 -0700

    Updated help to capture new commands

--- Modules/HelpCommands.cs
+++ Modules/HelpCommands.cs
@@ -12,6 +12,8 @@ namespace BugrudoBot.Modules
                 .WithTitle("BugrudoBot2.0 Help")
                 .WithFooter("I am using a real database and am written in dotnet core :)")
                 .AddField("!bug This is a test bug", "Logs a bug with the text \"This is a test bug\"", false)
+                .AddField("!deletebug 2", "Deletes the bug with id 2", false)
+                .AddField("!restorebug 2", "Restores the bug with id 2", false)
                 .AddField("!bugs", "Shows a list of all bugs", false);
 
             await Context.Channel.SendMessageAsync("", false, embedBuilder.Build());
